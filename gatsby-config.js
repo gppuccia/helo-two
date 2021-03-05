3326d25b3076aebf1d40136586bc04a955e7d348
gppuccia <m.pucciarelli@goproject.it>
2021-03-05 02:14:51 +0100

    Initial commit from gatsby

---
+++ gatsby-config.js
@@ -0,0 +1,18 @@
+module.exports = {
+  siteMetadata: {
+    title: "Helo Two",
+  },
+  plugins: [
+    "gatsby-plugin-netlify-cms",
+    "gatsby-plugin-theme-ui",
+    "gatsby-transformer-remark",
+    {
+      resolve: "gatsby-source-filesystem",
+      options: {
+        name: "pages",
+        path: "./src/pages/",
+      },
+      __key: "pages",
+    },
+  ],
+};
