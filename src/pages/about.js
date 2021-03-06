5b291cd042eaaa78dbccd768d7c123051600f850
Marcello Pucciarelli <m.pucciarelli@goproject.it>
2021-03-06 13:21:59 +0100

    Initial explorations.

---
+++ src/pages/about.js
@@ -0,0 +1,10 @@
+import React from "react";
+
+export default function About() {
+  return (
+    <div style={{ color: "#bbb" }}>
+      <h1>About this site</h1>
+      <p>About...</p>
+    </div>
+  );
+}
